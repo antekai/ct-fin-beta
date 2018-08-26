bb45152682f7ee4f38f35f2b7d38dd07839505a7
Andreas Teneketzoglou <antekai00@gmail.com>
2018-08-26 21:46:37 +0200

    add steps

--- src/components/Invoice/Invoice.js
+++ src/components/Invoice/Invoice.js
@@ -1,15 +1,70 @@
 import React from 'react';
+import { Steps, Button, message } from 'antd';
 import FileUpload from './File/Upload';
 import InputDateAmount from './Input/DateAmount';
+import './Invoice.css'
 
-
-
+const Step = Steps.Step;
 export default class Invoice extends React.Component{
+  
+  state={current:0}
+
+  next() {
+    const current = this.state.current + 1;
+    this.setState({ current });
+  }
+
+  prev() {
+    const current = this.state.current - 1;
+    this.setState({ current });
+  }
+
+
   render(){
+    const { current } = this.state;
     return(
-      <div>
-        <FileUpload/>
-        <InputDateAmount/>
+      <div className="invoice-container">
+        <Steps current={current}>
+          <Step title={'Add Invoice File'} />
+          <Step title={'Add Invoice Info'} />
+          <Step title={'Additional Files'} />
+        </Steps>
+        <div className="steps-content">
+          {
+            current===0 
+            &&  <div>
+                  <div className="invoice-title"> Upload Invoice File</div> 
+                  <FileUpload/>
+                </div> 
+          }
+          {
+            current===1 
+            && <InputDateAmount/>
+          }
+          {
+            current===2 
+            && <InputDateAmount/>
+          }
+        
+        </div>
+        <div className="steps-action">
+          {
+            current < 2
+            && <Button type="primary" onClick={() => this.next()}>Next</Button>
+          }
+          {
+            current === 2
+            && <Button type="primary" onClick={() => message.success('Processing complete!')}>Done</Button>
+          }
+          {
+            current > 0
+            && (
+            <Button style={{ marginLeft: 8 }} onClick={() => this.prev()}>
+              Previous
+            </Button>
+            )
+          }
+        </div>
       </div>
     )
   }
